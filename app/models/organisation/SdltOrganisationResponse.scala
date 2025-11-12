e44a3305ec6c4149a5825ab2891d0688252a1717
AyubAz <110174399+ayubAz@users.noreply.github.com>
2025-11-12 09:30:45 +0000

    FIX BE model

--- app/models/organisation/SdltOrganisationResponse.scala
+++ app/models/organisation/SdltOrganisationResponse.scala
@@ -20,52 +20,51 @@ import play.api.libs.functional.syntax.toFunctionalBuilderOps
 import play.api.libs.json.{JsError, JsNumber, JsString, JsSuccess, JsValue, Json, OFormat, OWrites, Reads, __}
 
 case class Agent(
-                  agentReference : String,
-                  name           : String,
-                  agentId        : Option[String],
-                  houseNumber    : Option[String],
-                  addressLine1   : Option[String],
-                  addressLine2   : Option[String],
-                  addressLine3   : Option[String],
-                  addressLine4   : Option[String],
-                  postcode       : Option[String],
-                  phone          : Option[String],
-                  email          : Option[String]
+                  agentReferenceNumber : String,
+                  name                 : String,
+                  agentId              : Option[String],
+                  houseNumber          : Option[String],
+                  addressLine1         : Option[String],
+                  addressLine2         : Option[String],
+                  addressLine3         : Option[String],
+                  addressLine4         : Option[String],
+                  postcode             : Option[String],
+                  phone                : Option[String],
+                  email                : Option[String]
                 )
 
 object Agent {
   implicit val reads: Reads[Agent] = (
-    (__ \ "agentReference").read[JsValue].map {
-      case JsNumber(n) if n.isWhole       => n.toBigInt.toString
-      case JsNumber(_)                    => "0"
+    (__ \ "agentReferenceNumber").read[JsValue].map {
+      case JsNumber(n) if n.isWhole => n.toBigInt.toString
+      case JsNumber(_) => "0"
       case JsString(s) if s.trim.nonEmpty => s.trim
-      case _                              => "0"
+      case _ => "0"
     } and
-      (__ \ "name")           .read[String] and
-      (__ \ "agentId")        .readNullable[String] and
-      (__ \ "houseNumber")    .readNullable[String] and
-      (__ \ "addressLine1")   .readNullable[String] and
-      (__ \ "addressLine2")   .readNullable[String] and
-      (__ \ "addressLine3")   .readNullable[String] and
-      (__ \ "addressLine4")   .readNullable[String] and
-      (__ \ "postcode")       .readNullable[String] and
-      (__ \ "phone")          .readNullable[String] and
-      (__ \ "email")          .readNullable[String]
+      (__ \ "name").read[String] and
+      (__ \ "agentId").readNullable[String] and
+      (__ \ "houseNumber").readNullable[String] and
+      (__ \ "addressLine1").readNullable[String] and
+      (__ \ "addressLine2").readNullable[String] and
+      (__ \ "addressLine3").readNullable[String] and
+      (__ \ "addressLine4").readNullable[String] and
+      (__ \ "postcode").readNullable[String] and
+      (__ \ "phone").readNullable[String] and
+      (__ \ "email").readNullable[String]
     )(Agent.apply _)
 
   implicit val writes: OWrites[Agent] = OWrites { a =>
     Json.obj(
-      "agentReference" -> (if (a.agentReference.trim.nonEmpty) a.agentReference.trim else "0"),
-      "name"           -> a.name,
-      "agentId"        -> a.agentId,
-      "houseNumber"    -> a.houseNumber,
-      "addressLine1"   -> a.addressLine1,
-      "addressLine2"   -> a.addressLine2,
-      "addressLine3"   -> a.addressLine3,
-      "addressLine4"   -> a.addressLine4,
-      "postcode"       -> a.postcode,
-      "phone"          -> a.phone,
-      "email"          -> a.email
+      "agentReferenceNumber" -> (if (a.agentReferenceNumber.trim.nonEmpty) a.agentReferenceNumber.trim else "0"),
+      "agentName" -> a.name,
+      "houseNumber" -> a.houseNumber.getOrElse(""),
+      "addressLine1" -> a.addressLine1.getOrElse(""),
+      "addressLine2" -> a.addressLine2,
+      "addressLine3" -> a.addressLine3.getOrElse(""),
+      "addressLine4" -> a.addressLine4,
+      "postcode" -> a.postcode,
+      "phone" -> a.phone,
+      "email" -> a.email.getOrElse("")
     )
   }
 }
