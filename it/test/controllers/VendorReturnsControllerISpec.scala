ccc1e2942b64e927505fcfacb2613d651debc297
avelx <3528155+avelx@users.noreply.github.com>
2026-01-23 09:35:34 +0000

    DTR-2386: ** test: add VendorReturnsControllerISpec | cover updateVendor method

--- it/test/controllers/VendorReturnsControllerISpec.scala
+++ it/test/controllers/VendorReturnsControllerISpec.scala
@@ -16,11 +16,10 @@
 
 package controllers
 
-//class  {
-
 import base.BaseSpec
 import itutil.ApplicationWithWiremock
-import models.filing.{CreateVendorRequest, CreateVendorReturn, ReturnVersionUpdateRequest, ReturnVersionUpdateReturn}
+import models.filing.{CreateVendorRequest, CreateVendorReturn, ReturnVersionUpdateRequest, ReturnVersionUpdateReturn, UpdateVendorRequest, UpdateVendorReturn}
+import org.apache.pekko.http.scaladsl.model.HttpHeader.ParsingResult.Ok
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.http.Status
 import play.api.http.Status.{CREATED, FORBIDDEN}
@@ -32,10 +31,16 @@ class VendorReturnsControllerISpec extends BaseSpec
 
   val servicePrefix = s"http://localhost:$port/stamp-duty-land-tax"
   lazy val createVendorUrl = s"$servicePrefix/filing/create/vendor"
+  lazy val updateVendorUrl = s"$servicePrefix/filing/update/vendor"
 
   def stubCreateVendorResponse(): Unit = {
     stubPost("/formp-proxy/filing/create/vendor", Status.CREATED,
       Json.toJson(CreateVendorReturn(vendorResourceRef = "ref", vendorId = "vendorId")).toString)
+  }
+
+  def stubUpdateVendorResponse(): Unit = {
+    stubPost("/formp-proxy/filing/update/vendor", Status.CREATED,
+      Json.toJson(UpdateVendorReturn(updated = true)).toString)
   }
 
   "VendorReturns" should {
@@ -70,6 +75,46 @@ class VendorReturnsControllerISpec extends BaseSpec
             postcode = None, isRepresentedByAgent = "isRep"))
 
         val result = wsClient.url(createVendorUrl)
+          .withHttpHeaders("Authorization" -> "Bearer123")
+          .post(jsonBody)
+
+        result.status shouldBe CREATED
+      }
+
+    }
+
+    "call updateVendor" when {
+
+      "return a 403:Forbidden:: authorised request" in {
+        stubUnauthorised()
+        stubUpdateVendorResponse()
+        val jsonBody = Json.toJson(
+          UpdateVendorRequest(stornId = "storn", returnResourceRef = "ref",
+            title = None, forename1 = None, forename2 = None, name = "name",
+            houseNumber = None, addressLine1 = "address1", addressLine2 = None,
+            addressLine3 = None, addressLine4 = None,
+            postcode = None, isRepresentedByAgent = "isRep", vendorResourceRef = "ref",
+            nextVendorId = None))
+
+        val result = wsClient.url(updateVendorUrl)
+          .withHttpHeaders("Authorization" -> "Bearer123")
+          .post(jsonBody)
+
+        result.status shouldBe FORBIDDEN
+      }
+
+      "return a 200:OK:: authorised request" in {
+        stubAuthorisedAsActivated()
+        stubUpdateVendorResponse()
+        val jsonBody = Json.toJson(
+          UpdateVendorRequest(stornId = "storn", returnResourceRef = "ref",
+            title = None, forename1 = None, forename2 = None, name = "name",
+            houseNumber = None, addressLine1 = "address1", addressLine2 = None,
+            addressLine3 = None, addressLine4 = None,
+            postcode = None, isRepresentedByAgent = "isRep", vendorResourceRef = "ref",
+            nextVendorId = None))
+
+        val result = wsClient.url(updateVendorUrl)
           .withHttpHeaders("Authorization" -> "Bearer123")
           .post(jsonBody)
 
